c447bda4b09bcef8c02788256c46d5e592919738
Saksham Sharma <sakshamshankyan7@gmail.com>
2024-08-31 22:07:56 +0530

    initial commit

---
+++ LMS-spring-boot-be/lmsproject/src/main/java/com/lms/lmsproject/LmsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.lms.lmsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsprojectApplication.class, args);
+	}
+
+}
---
+++ LMS-spring-boot-be/lmsproject/src/test/java/com/lms/lmsproject/LmsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lms.lmsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
